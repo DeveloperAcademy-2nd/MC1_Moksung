34fa834f4b30240d8d2cfca755a22a2f86e92330
Musk <yuiop1029@naver.com>
2023-03-29 16:41:44 +0900

    1-1 : 메인페이지 네비게이션 뷰 추가.

--- Jupiter.swiftpm/Episode1/Episode1.swift
+++ Jupiter.swiftpm/Episode1/Episode1.swift
@@ -1,29 +1,27 @@
 //
-//  Episode1.swift
+//  SwiftUIView.swift
 //  
 //
-//  Created by 한지석 on 2023/03/27.
+//  Created by A_Mcflurry on 2023/03/29.
 //
 
 import SwiftUI
 
 struct Episode1: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Rectangle()
-                .textField("   대망의 첫 CBL,\n첫 MC1 시작이다.")
-        }
-        .background(Color.backgroundColor)
-    }
-}
-    
-
+        NavigationView(){
+            NavigationLink(destination: Ep1Frame1(),
+                           label: {
+                Text("Next")
+            })
+            
+        }   // navigationview
+        
+    }   // body
+}   // ep1 view
 
 struct Episode1_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            Episode1()
-        }
+        Episode1()
     }
 }
