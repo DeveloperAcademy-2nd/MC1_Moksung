72bf6a048def7867671ef8dd7af5f3006bc31563
Lets <ruvan123@naver.com>
2023-03-30 02:52:26 +0900

    2-2: View 생성 및 사진과 글 추가

---
+++ Jupiter.swiftpm/Episode2/Episode2Frame2.swift
@@ -0,0 +1,49 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by user on 2023/03/29.
+//
+
+import SwiftUI
+
+struct Episode2Frame2: View {
+    var body: some View {
+        ZStack {
+                Image("2_2")
+                .resizable()
+                .scaledToFit()
+            VStack {
+                Spacer()
+                Rectangle()
+                    .textField("우리는 결론을 도출하지 못하고, 결국 블랙보드 앞으로 향했다.")
+                    .overlay {
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                // button 클릭될 경우 text 변하도록 설정
+                                Button {
+                                    print("tapped")
+                                } label: {
+                                    Label("NEXT", systemImage: "arrow")
+                                }
+                                .frame(width: 50, height: 30)
+                                .padding(.bottom, 100)
+                                .padding(.trailing, 35)
+                                
+                            }
+                        }
+                    }
+            }
+        }
+        .background(Color.backgroundColor)
+        .ignoresSafeArea(.all)
+    }
+}
+
+struct Episode2Frame2_previews: PreviewProvider {
+    static var previews: some View {
+        Episode2Frame2()
+    }
+}
